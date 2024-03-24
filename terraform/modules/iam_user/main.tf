d3af3bfa6cfa51c7ea2266c9ea9791311fb3552e
uncaffe9 <164398259+uncaffe9@users.noreply.github.com>
2024-03-24 10:59:55 +0530

    Delete terraform directory

--- terraform/modules/iam_user/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_iam_user" "this" {
-  count = var.count_users
-
-  name = var.tags[count.index]["name"]
-  tags = var.tags[count.index]["tags"]
-}
-
--- terraform/modules/iam_user/output.tf
+++
@@ -1,8 +0,0 @@
-output "user_names" {
-  value = aws_iam_user.this[*].name
-}
-
-output "user_tags" {
-  description = "Tags applied to IAM users"
-  value       = var.tags
-}
